f237c335b3ebf951aeee7f7e098ae662b42b107b
Elizabeth Rivera <elizabeth.trigueros10@gmail.com>
2019-10-29 17:47:06 -0600

    Modificar y crear métodos
    como crear o modicar métodos privados o públicos

--- CursoUdemyCSharp/CursoUdemyCSharp/Program.cs
+++ CursoUdemyCSharp/CursoUdemyCSharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace CursoUdemyCSharp
 {
@@ -6,26 +7,23 @@ namespace CursoUdemyCSharp
     {
         static void Main(string[] args)
         {
-            //Manipular información de cadenas dede texto.
+            //Modificar y Crear métodos
 
-            //Una subcadena es cualquier secuencia de caracteres que está contenida en una cadena.
-            var curso = "Curso de C# desde cero";
-            var name = "Curso de C# desde cero y a empezar";
-            //Console.WriteLine("Cadena: {0}", curso.Substring(9, 11));
-            var sub = curso.Remove(5, 11);                                                          //Elimina parte de la cadena
-            //Console.WriteLine("Cadena: {0}", sub);
-            //var data = curso.IndexOf("desde");                                                   //Cuenta el número de caracteres que se encuentran antes de llegar al caracter indicado (unicamente utilizando comillas simples se ingresa un caracter, mientras que con comillas dobles se puede ingresar una palabra completa)
-            //Console.WriteLine("Cadena: {0}", data);
-            //var data = curso.Equals(name);                                                          //Compara el tamaño de la cadena name y curso, manda true o false
-            //Console.WriteLine("Cadena: {0}", data);
-            
-            //El foreach itera la cedan de texto
-            foreach (var item in curso)
-            {
-                Console.WriteLine(item);
-            }
+            var data = new Program();
+            //data.metodoPrivado();
+            Console.WriteLine(data.metodoPrivado());
             Console.ReadLine();
-            
+
+
         }
+        private string metodoPrivado()
+        {
+            return $"Eli Rivera {26}";
+        }
+        /*public void metodoPublico()
+        {
+            Console.WriteLine("Eli");
+            Console.ReadLine();
+        }*/
     }
 }
