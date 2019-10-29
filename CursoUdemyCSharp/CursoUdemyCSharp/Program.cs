cf358f1ccd6c912a8f1f5e19947c9d3512ee50fb
Elizabeth Rivera <elizabeth.trigueros10@gmail.com>
2019-10-29 13:00:21 -0600

    Manipulación de cadenas
    La utilización de funciones que permiten manipular cadenas, así como la manera de iterar usando un foreach.

--- CursoUdemyCSharp/CursoUdemyCSharp/Program.cs
+++ CursoUdemyCSharp/CursoUdemyCSharp/Program.cs
@@ -6,27 +6,26 @@ namespace CursoUdemyCSharp
     {
         static void Main(string[] args)
         {
-            /*var (a, b, opcion) = (3, 5, "+");
-            var result = opcion switch
+            //Manipular información de cadenas dede texto.
+
+            //Una subcadena es cualquier secuencia de caracteres que está contenida en una cadena.
+            var curso = "Curso de C# desde cero";
+            var name = "Curso de C# desde cero y a empezar";
+            //Console.WriteLine("Cadena: {0}", curso.Substring(9, 11));
+            var sub = curso.Remove(5, 11);                                                          //Elimina parte de la cadena
+            //Console.WriteLine("Cadena: {0}", sub);
+            //var data = curso.IndexOf("desde");                                                   //Cuenta el número de caracteres que se encuentran antes de llegar al caracter indicado (unicamente utilizando comillas simples se ingresa un caracter, mientras que con comillas dobles se puede ingresar una palabra completa)
+            //Console.WriteLine("Cadena: {0}", data);
+            //var data = curso.Equals(name);                                                          //Compara el tamaño de la cadena name y curso, manda true o false
+            //Console.WriteLine("Cadena: {0}", data);
+            
+            //El foreach itera la cedan de texto
+            foreach (var item in curso)
             {
-                "+" => a + b ,
-                "-" => a - b,
-                "*" => a * b
-            };*/
-            var valor = true;
-            var count = 1;
-            while (valor)
-            {
-                if (count == 6)
-                {
-                    valor = false;
-                }
-                count++;
-                Console.Write("Ejecucion {0}", count);
+                Console.WriteLine(item);
             }
-
-
             Console.ReadLine();
+            
         }
     }
 }
