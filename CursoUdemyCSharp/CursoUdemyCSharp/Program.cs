bead61ff0769d1b15ac061058d8560454a30927e
Elizabeth Rivera <elizabeth.trigueros10@gmail.com>
2019-10-31 11:47:48 -0600

    Funciones y arreglos
    Se creó una función y se observó su comportamiento.
    Datos duplicados en un arreglo.

--- CursoUdemyCSharp/CursoUdemyCSharp/Program.cs
+++ CursoUdemyCSharp/CursoUdemyCSharp/Program.cs
@@ -7,23 +7,53 @@ namespace Curso_de_net_core
     {
         static void Main()
         {
-            int data;
-            new Program().metodo(out data);
-            Console.WriteLine(data);
-            Console.ReadLine();
-        //    int edad = 26;
-        //    int data = 57;
-        //    new Program().metodo(edad);
-        //    Console.ReadLine();
-        //}
-        //private void metodo(in int valor)
-        //{
-        //    Console.WriteLine(valor);
-        }
-        private void metodo(out int valor)
-        {
-            valor = 50 + 20;
-        }
+            //    int data;
+            //    new Program().metodo(out data);
+            //    Console.WriteLine(data);
+            //    Console.ReadLine();
+            //}
 
+            //private void metodo(out int valor)
+            //{
+            //    valor = funcion();
+
+            //    int funcion()
+            //    {
+            //        return 50 + 30;
+            //    }
+
+            int[] array = { 1, 2, 2, 3, 3, 3 };
+            int[] list = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        count++;
+                        if (numero(array[i]))
+                        {
+                            list[i] = array[i];
+                        }
+                    }
+                }
+                if (list[i] != 0)
+                {
+                    Console.WriteLine(list[i] + " se repite: " + count);
+                }
+            }
+            bool numero(int num)
+            {
+                for (int i = 0; i < list.Length; i++)
+                {
+                    if (list[i] == num)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
     }
 }
