83ad3da87fe8491cf356adfc6d4ac8511cf7ec86
Elizabeth Rivera <elizabeth.trigueros10@gmail.com>
2019-11-04 12:10:39 -0600

    Herencias de clases
    Se creó la clase Menu, sobre la cual se heredan atributos a la subclase Program, también se utilizaron las palabras:
    protected: protege un elemento o método de una clase.
    base: Hace referencia al método constructor de la clase que se esta heredando.
    override: Sobreescribe en la clase que se está heredando.
    virtual: indica que el método será sobreescrito de la clase derivada.

---
+++ CursoUdemyCSharp/CursoUdemyCSharp/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoUdemyCSharp
+{
+    public class Menu
+    {
+        private string s;
+        public Menu(string s)
+        {
+            this.s = s;
+        }
+        protected virtual void getProducto()
+        {
+            Console.WriteLine(s);
+        }
+        //public void getProducto()
+        //{
+        //    Console.WriteLine("Ya casi es mu cumple :)");
+        //}
+    }
+}
--- CursoUdemyCSharp/CursoUdemyCSharp/Program.cs
+++ CursoUdemyCSharp/CursoUdemyCSharp/Program.cs
@@ -4,45 +4,23 @@ using System.Text;
 
 namespace CursoUdemyCSharp
 {
-    class Program
+    class Program : Menu
     {
+        public Program(String s) : base(s)
+        {
+
+        }
         static void Main()
         {
-            //var data = new Estudiante();
-            //data.Nombre = "Eli";
-            List<Object> lista = new List<Object>();
-            lista.Add("Eli");
-            lista.Add(32);
-            lista.Add(true);
-            //lista.Remove(32);
-            //lista.ForEach(item => Console.WriteLine(item));
-            lista.Insert(2, "Andy");
-            //lista.Clear();
-            lista.Reverse();
-            Console.WriteLine(lista.Exists(e => e.Equals("Eli")));
-            foreach(var item in lista)
-            {
-                Console.WriteLine(item);
-            }
-            //for(int i = 0; i < lista.Count; i++)
-            //{
-            //    Console.WriteLine(lista[i]);
-            //}
-
-            //int j = 0;
-            //for(; ; )
-            //{
-            //    if (j < lista.Count)
-            //    {
-            //        Console.WriteLine(lista[j]);
-            //        j++;
-            //    }
-            //    else
-            //        break;
-            //}
-           // Console.WriteLine(lista.Count);
+            new Program("Ya casi es mi cumple").getProducto();
             Console.ReadKey();
+            //new Program().getProducto();
+            //Console.ReadKey();
 
+        }
+        protected override void getProducto()
+        {
+            Console.WriteLine("El 10 de enero");
         }
     }
 }
